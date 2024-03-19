ed5db51be281b6603db87c49959cb92da5057c69
jiyoung-0y0 <kimwldud1234@gmail.com>
2024-03-19 18:53:17 +0900

    스캐너

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("== 프로젝트 시작 ==");
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        String cm = sc.nextLine();
+        int num = sc.nextInt();
+        System.out.printf("입력된 명령어 : %s %s %d\n", cmd, cm, num);
+        sc.close();
+        System.out.println("== 프로젝트 끝 ==");
     }
 }
