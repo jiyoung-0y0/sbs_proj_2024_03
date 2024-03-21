9a8fb3d3de5a81cb02408215172f243c97ade161
jiyoung-0y0 <kimwldud1234@gmail.com>
2024-03-21 14:14:18 +0900

    게시물수정기능추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -52,9 +52,37 @@ public class Main {
 
                 }
             }
-            else if (cmd.startsWith("article detail")){
+            else if (cmd.startsWith("article modify")){
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]);
+
+                    Article foundArticle = null;
+
+                    for (int i=0; i<articles.size(); i++){
+                        Article article = articles.get(i);
+
+                        if(article.id == id){
+                            foundArticle = article;
+                            break;
+                        }
+                    }
+                    if (foundArticle == null){
+                        System.out.printf("%d번 게시물은 존재하지 않습니다.\n",id);
+                        continue;
+                    }
+
+                    System.out.printf("제목: %s\n",foundArticle.title);
+                    String title = sc.nextLine();
+                    System.out.printf("내용: %s\n",foundArticle.body);
+                    String body = sc.nextLine();
+                    foundArticle.title = title;
+                    foundArticle.body = body;
+
+                    System.out.printf("%d번 게시물이 수정되었습니다.\n",id);
+                }
+                else if (cmd.startsWith("article detail")){
+                    String[] cmdBits = cmd.split(" ");
+                    int id = Integer.parseInt(cmdBits[2]);
 
                     Article foundArticle = null;
 
